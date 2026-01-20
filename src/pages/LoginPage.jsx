bb295b44d33fd9a557456acc50d0c44707481f81
boris <boradmy@gmail.com>
2026-01-20 11:20:36 -0500

    cammbios

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,38 @@
 import { Box, Typography, TextField, Button } from '@mui/material';
 import { login } from '../services/authServices';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
+  const navigate = useNavigate();
+  const [loginData, setLoginData] = useState({
+    username: '',
+    password: ''
+  });
+ 
+  const handleChange = (e) => {
+    setLoginData({
+      ...loginData,
+      [e.target.name]: e.target.value
+    });
+  };
 
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    const responseData = await login(loginData.username, loginData.password);
+    localStorage.setItem('access_token', responseData.access_token);
+    alert("Inicio de sesión exitoso");
+    navigate('/');
+  } catch (error) {
+    console.error("Error during login:", error);
+    alert("Error al iniciar sesión");
+  }
+};
 
-    
   return (
     <Box
-      component="form"
+      component="form" onSubmit={handleSubmit}
       sx={{
         display: 'flex',
         flexDirection: 'column',
@@ -23,6 +48,8 @@ export default function LoginPage() {
         label="Usuario"
         name="username"
         variant="outlined"
+        value={loginData.username}
+        onChange={handleChange}
         required
       />
       <TextField
@@ -30,6 +57,8 @@ export default function LoginPage() {
         name="password"
         type="password"
         variant="outlined"
+        value={loginData.password}
+        onChange={handleChange}
         required
       />
       <Button type="submit" variant="contained" color="primary">
